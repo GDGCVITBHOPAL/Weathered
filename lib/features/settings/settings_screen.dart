9a35baba92973b1742e4ac46d5659086d30d6464
Gyan Vardhan <115806215+TheNoob51@users.noreply.github.com>
2024-02-22 15:41:34 +0530

    add settings

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:weathered/constants/style.dart';
+import 'package:weathered/features/settings/switch.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -15,6 +19,76 @@ class SettingsScreen extends StatelessWidget {
             style: AppStyle.textTheme.titleLarge,
           ),
         ),
+        Gap(10),
+
+        //For Dark Mode
+        // The Slider is left
+        Container(
+          margin: EdgeInsets.fromLTRB(16, 16, 16, 5),
+          decoration: BoxDecoration(
+              color: Color(0x3897ABFF),
+              borderRadius: BorderRadius.circular(15.0)),
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Icon(Icons.nightlight_round, size: 35.0),
+              Gap(10.0),
+              Text("Dark Mode", style: AppStyle.textTheme.labelMedium),
+
+              // this is done to keep the Switch Widget to left
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SwitchMode(),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+
+        //Temperature Unit
+        //The change method is left to be implemented
+        Container(
+          margin: EdgeInsets.fromLTRB(16, 16, 16, 5),
+          decoration: BoxDecoration(
+              color: Color(0x3897ABFF),
+              borderRadius: BorderRadius.circular(15.0)),
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Icon(Icons.thermostat_outlined, size: 35.0),
+              Gap(10.0),
+              Text("Temperature Units", style: AppStyle.textTheme.labelMedium)
+            ],
+          ),
+        ),
+
+        //Send Feedback
+        Container(
+          margin: EdgeInsets.fromLTRB(16, 16, 16, 5),
+          decoration: BoxDecoration(
+              color: Color(0x3897ABFF),
+              borderRadius: BorderRadius.circular(15.0)),
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Icon(Icons.mail_outline_rounded, size: 35.0),
+              Gap(10.0),
+              Text("Send Feedback", style: AppStyle.textTheme.labelMedium)
+            ],
+          ),
+        ),
+
+        // Made with Love
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [Text("Made With <3 GDSC VITB"), Gap(20.0)],
+          ),
+        )
       ],
     ));
   }
