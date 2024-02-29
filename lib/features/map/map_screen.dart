83cca3d6a1e775359469c86530e0f24d819e6700
Gyan Vardhan <115806215+TheNoob51@users.noreply.github.com>
2024-02-29 20:52:16 +0530

    geolocation package import and google map integration

--- lib/features/map/map_screen.dart
+++ lib/features/map/map_screen.dart
@@ -1,13 +1,33 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class MapScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
   @override
+  State<MapScreen> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapScreen> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _initialPos = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 4,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Map Screen"),
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _initialPos,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
